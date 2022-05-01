4a74fe1566478fecd66e25560bea57f7b15e106e
AHMED NABIB <nabibpallab@gmail.com>
2022-05-01 14:20:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const score0El = document.querySelector('#score--0');
+const Score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice')
+
+score0El.textContent = 0;
+Score1El.textContent = 0;
+diceEl.classList.add('hidden')
+
